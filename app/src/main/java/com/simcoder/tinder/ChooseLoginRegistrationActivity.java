f0f9bcbd32d7dccd40f120bba0b283160517ed1a
oriyafr <oriyafr@github.cs.huji.ac.il>
2018-04-27 16:58:20 +0300

    last version. chat not working

--- app/src/main/java/com/simcoder/tinder/ChooseLoginRegistrationActivity.java
+++ app/src/main/java/com/simcoder/tinder/ChooseLoginRegistrationActivity.java
@@ -1,12 +1,10 @@
 package com.simcoder.tinder;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class ChooseLoginRegistrationActivity extends AppCompatActivity {
 
@@ -15,13 +13,7 @@ public class ChooseLoginRegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_login_registration);
-        Button button1 = (Button) findViewById(R.id.login);
-        Button button2 = (Button) findViewById(R.id.register);
-        Typeface typeface = Typeface.createFromAsset(getAssets(),"Raleway-Light.ttf");
-        button1.setText("Login");
-        button1.setTypeface(typeface);
-        button2.setText("Register");
-        button2.setTypeface(typeface);
+
         mLogin = (Button) findViewById(R.id.login);
         mRegister = (Button) findViewById(R.id.register);
 
@@ -43,6 +35,7 @@ public class ChooseLoginRegistrationActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
                 return;
+
             }
         });
     }
